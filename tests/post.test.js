a63f8306273eefa1785381e26beeac1fde6e7822
YamiNar20 <128421560+YamiNar20@users.noreply.github.com>
2023-07-13 01:04:01 -0400

    Update post.test.js

--- tests/post.test.js
+++ tests/post.test.js
@@ -1,122 +1,112 @@
-const request = require('supertest')
-const mongoose = require('mongoose')
-const { MongoMemoryServer } = require('mongodb-memory-server')
-const app  = require('../../app')
-const server = app.listen(8080, () => console.log('Testing on PORT 8080'))
-const Post = require('../models/post')
+const request = require('supertest');
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const app = require('../app');
+const Post = require('../models/post');
 
-let mongoServer
+let mongoServer;
+let Server;
 
 beforeAll(async () => {
-  mongoServer = await MongoMemoryServer.create()
-  await mongoose.connect(mongoServer.getUri(), { useNewUrlParser: true, useUnifiedTopology: true })
-})
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+  await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+  Server = app.listen(8080, () => {
+    console.log('Testing on PORT 8080');
+  });
+});
 
 afterAll(async () => {
-  await mongoose.connection.close()
-  mongoServer.stop()
-  server.close()
-})
+  await mongoose.connection.close();
+  await mongoServer.stop();
+  Server.close();
+});
 
-afterAll((done) => done())
+describe('Test the post endpoints', () => {
+  test('It should create a new post', async () => {
+    const postData = {
+      title: 'Test Post',
+      content: 'This is a test post',
+    };
 
+    const response = await request(app)
+      .post('/posts')
+      .send(postData)
+      .expect(201);
 
-  beforeEach(async () => {
-    // Clear the posts collection before each test
-    await Post.deleteMany({});
+    const createdPost = await Post.findById(response.body._id);
+    expect(createdPost.title).toBe(postData.title);
+    expect(createdPost.content).toBe(postData.content);
   });
+});
+test('Get request to get all posts', async () => {
+  const postData = {
+    title: 'Test Post',
+    content: 'This is a test post',
+  };
 
-  describe('POST /posts', () => {
-    it('should create a new post', async () => {
-      const postData = {
-        user: 'user_id',
-        title: 'Test Post',
-        description: 'This is a test post'
-      };
+  const response = await request(app)
+    .post('/posts')
+    .send(postData)
+    .expect(201);
 
-      const response = await request(app)
-        .post('/posts')
-        .send(postData)
-        .expect(201);
+  const createdPost = await Post.findById(response.body._id);
+  expect(createdPost.title).toBe(postData.title);
+  expect(createdPost.content).toBe(postData.content);
+});
 
-      expect(response.body.title).toBe(postData.title);
-      expect(response.body.description).toBe(postData.description);
-    });
+test('PUT request to update a specific post', async () => {
+  // Create a new post
+  const newPostData = {
+    title: 'Test Post',
+    content: 'This is a test post',
+  };
 
-    it('should return an error if required fields are missing', async () => {
-      const postData = {
-        // Missing required fields: user, title, description
-      };
+  const createResponse = await request(app)
+    .post('/posts')
+    .send(newPostData)
+    .expect(201);
 
-      const response = await request(app)
-        .post('/posts')
-        .send(postData)
-        .expect(400);
+  const postId = createResponse.body._id;
+// Update the post
+const updateData = {
+  title: 'Updated Post',
+  content: 'This post has been updated',
+};
 
-      expect(response.body.error).toBe('Missing required fields');
-    });
-  });
+const updateResponse = await request(app)
+  .put(`/posts/${postId}`)
+  .send(updateData)
+  .expect(200);
 
-  describe('GET /posts/:postId', () => {
-    it('should get a post by ID', async () => {
-      const testPost = new Post({
-        user: 'user_id',
-        title: 'Test Post',
-        description: 'This is a test post'
-      });
 
-      await testPost.save();
+const updatedPost = await Post.findById(postId);
 
-      const response = await request(app)
-        .get(`/posts/${testPost._id}`)
-        .expect(200);
+expect(updatedPost.title).toBe(updateData.title);
+expect(updatedPost.content).toBe(updateData.content);
+});
 
-      expect(response.body._id).toBe(testPost._id.toString());
-      expect(response.body.title).toBe(testPost.title);
-      expect(response.body.description).toBe(testPost.description);
-    });
+test('DELETE request to delete a specific post', async () => {
+  // Create a new post
+  const newPostData = {
+    title: 'Test Post',
+    content: 'This is a test post',
+  };
 
-    it('should return an error if post not found', async () => {
-      const nonExistingPostId = 'non_existing_id';
+  const createResponse = await request(app)
+    .post('/posts')
+    .send(newPostData)
+    .expect(201);
 
-      const response = await request(app)
-        .get(`/posts/${nonExistingPostId}`)
-        .expect(404);
+  const postId = createResponse.body._id;
 
-      expect(response.body.error).toBe('Post not found');
-    });
-  });
+  // Delete the post
+  await request(app)
+    .delete(`/posts/${postId}`)
+    .expect(200);
 
- 
-  test('DELETE /posts/:postId', () => {
-    it('should delete a post by ID', async () => {
-      // Create a test post
-      const testPost = new Post({
-        user: 'user_id',
-        title: 'Test Post',
-        description: 'This is a test post'
-      });
-      await testPost.save();
+  // Verify the post is deleted
+  const deletedPost = await Post.findById(postId);
+  expect(deletedPost).toBeNull();
+});
 
-      // Send a delete request to delete the post
-      const response = await request(app)
-        .delete(`/posts/${testPost._id}`)
-        .expect(200);
-
-      expect(response.body.message).toBe('Post deleted successfully');
-
-      // Check if the post was deleted from the database
-      const deletedPost = await Post.findById(testPost._id);
-      expect(deletedPost).toBeNull();
-    });
-
-    it('should return an error if post not found', async () => {
-      const nonExistingPostId = 'non_existing_id';
-
-      const response = await request(app)
-        .delete(`/posts/${nonExistingPostId}`)
-        .expect(404);
-
-      expect(response.body.error).toBe('Post not found');
-    });
-  });
